961bcbc0dc9d49cd34255ff4849c9e0ea3bce4a5
Gerson <gdmunhos@gmail.com>
2021-10-29 22:58:42 -0300

    Atividade_Limpando_R

--- 3_R_Tratamento_Dados/limpando_1.R
+++ 3_R_Tratamento_Dados/limpando_1.R
@@ -1,7 +1,11 @@
+setwd("/home/gconcilio/Documents/Programação/Curso Data Science/python_and_r/3_R_Tratamento_Dados")
+
 dados <- read.csv("Churn.csv", sep = ";", na.strings = "", stringsAsFactors = T)
+
 colnames(dados) <- c("Id", "Score", "Estado", "Genero", "Idade", "Patrimonio", "Saldo", "Produtos", "Tem_Cart_Credito",
  "Ativo", "Salario", "Saiu")
 
+ dados
 
 #-----------TRATANDO  SALÁRIO ----------------
 print("\n")
@@ -35,5 +39,28 @@ dados[dados$Idade < 0 | dados$Idade > 110, ]$Idade <- median(dados$idade)
 
 
 #-----------DADOS DUPLICADOS ----------------
-dados[duplicated(dados$Id), ]
-dados
+x <- dados[duplicated(dados$Id), ]
+x$Id
+dados <- dados[-c(x$Id), ]
+dados[dados$Id == x$Id, ]
+
+
+#-----------ESTADOS FORA DO DOMINIO ----------------
+unique(dados$Estado)
+summary(dados$Estado)
+
+dados[!dados$Estado %in% c("RS", "SC", "PR", "SP"), ]$Estado <- "RS" 
+dados$Estado <- factor(dados$Estado)
+
+
+#-----------OUTLIERS----------------
+des <- sd(dados$Salario, na.rm = T)
+des
+
+dados[dados$Salario >= (2 * des), ]$Id
+boxplot(dados$Salario)
+
+mediana <- median(dados$Salario)
+mediana
+
+dados[dados$Salario >= des, ]$Salario <- mediana
